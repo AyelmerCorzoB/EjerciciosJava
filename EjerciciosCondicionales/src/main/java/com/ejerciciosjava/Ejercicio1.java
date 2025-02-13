7e4406e8abf5e7fd71c6ff98c1a63402f7714319
AyelmerCorzoB <corzobarbosaa@gmail.com>
2025-02-12 20:53:41 -0500

    fix: :bug: Se corrigieron las condiciones de edad en Ejercicio1.java para una mejor clasificación

--- EjerciciosCondicionales/src/main/java/com/ejerciciosjava/Ejercicio1.java
+++ EjerciciosCondicionales/src/main/java/com/ejerciciosjava/Ejercicio1.java
@@ -10,11 +10,11 @@ public class Ejercicio1 {
             System.out.print("Ingresa tu edad: ");
             edad = input.nextInt();
         }
-        if (edad > 0 || edad < 12) {
+        if (edad >= 0 && edad <= 12) {
             System.out.println("Eres un niño porque estas entre los (0 - 12) años");
-        } else if (edad > 13 || edad < 17) {
+        } else if (edad >= 13 && edad <= 17) {
             System.out.println("Eres un adolecente porque estas entre los (13 - 17) años");
-        } else if (edad >18 || edad < 59) {
+        } else if (edad >= 18 && edad <= 59) {
             System.out.println("Eres un adulto porque estas entre los (18 - 59) años");
         } else {
             System.out.println("Eres un adulto mayor ya que tienes (60+) años");
