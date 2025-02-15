c1a2a6c56f131901740a47bdadf94ef09e6fffc6
AyelmerCorzoB <corzobarbosaa@gmail.com>
2025-02-15 09:42:22 -0500

    feat: :white_check_mark: Ejercicio en clase mejorado y actualizado con la funcion de repetir el caso
    con el ciclo do while dentro del caso se mejoro la funcionalidad del programa para que el usuario repita el mismo caso hasta que la condicion sea diferente a S o s
    
    By @AyelmerCorzoB

--- EjercicioEnClase/ejercicio/src/main/java/com/ejerciciosenclase/Main.java
+++ EjercicioEnClase/ejercicio/src/main/java/com/ejerciciosenclase/Main.java
@@ -4,62 +4,82 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        boolean valido = false;
-        String opcionSalir;
-        String menu = """
-                =============================================
-                  SIMULADOR DE PROPINA
-                =============================================
-                1. Calcular propina y total a pagar
-                2. Calcular total a pagar divido entre varias personas
-                3. Salir
-                =============================================
-                                """;
-        String menu1 = """
-                =============================================
-                    Cálculo de Propina
-                =============================================
-                """;
-        String menu2 = """
-                =============================================
-                    Dividir Cuenta entre Varias Personas
-                =============================================
-                """;
+        try (Scanner sc = new Scanner(System.in)) {
+            String menu = """
+                    =============================================
+                    \tSIMULADOR DE PROPINA
+                    =============================================
+                    1. Calcular propina y total a pagar
+                    2. Calcular total a pagar divido entre varias personas
+                    3. Salir
+                    =============================================
+                    Por favor, elige una opción (1-3): """;
+            String menu1 = """
+                    =============================================
+                    \tCálculo de Propina
+                    =============================================
+                    """;
+            String menu2 = """
+                    =============================================
+                    \tDividir Cuenta entre Varias Personas
+                    =============================================
+                    """;
+            int opcion;
+            do {
+                System.out.print(menu);
+                opcion = sc.nextInt();
+                switch (opcion) {
+                    case 1:
+                        char repetir;
+                        do {
 
-        do {
-            System.out.println(menu);
-            System.out.print("Por favor, elige una opcion (1-3): ");
-            int opcion = sc.nextInt();
-            switch (opcion) {
-                case 1:
-                    System.out.println(menu1);
-                    System.out.print("Ingrese l monto total de la cuenta : $");
-                    double montoCuenta = sc.nextDouble();
-                    System.out.print("Ingrese el porcentaje de propina(por ejemplo: 15)");
-                    int porcentaje = sc.nextInt();
-                    System.out.println("=============================================");
-                    double resultado = montoCuenta * porcentaje;
-                    System.out.println("La propina calculada es: $" + resultado);
-                    double resultadofinal = resultado + montoCuenta;
-                    System.out.println("El total es: $" + resultadofinal);
-                    System.out.println("=============================================");
-                    System.out.println("¿Deseas calcular nuevamente? (S/N)");
-                    opcionSalir = sc.nextLine();
-                    if (opcionSalir.equals("s") || opcionSalir.equals("S")) {
-                        valido = true;
-                    }
-                    break;
-                case 2:
-                    System.out.println("Calcular la propina y dividir la cuenta entre varias personas");
-                    break;
-                case 3:
-                    System.out.println("Salir...");
-                default:
+                            System.out.print(menu1);
+                            System.out.print("Ingrese el monto total de la cuenta: $");
+                            double montoP = sc.nextDouble();
+                            System.out.print("Ingrese el porcentaje de propina (por ejemplo: 15): ");
+                            int porcentaje = sc.nextInt();
+                            System.out.println("=============================================");
+                            double propinaCalculada = montoP * porcentaje / 100;
+                            double totalPagar = montoP + propinaCalculada;
+                            System.out.println("La propina calculada es: $" + propinaCalculada);
+                            System.out.println("El total a pagar es: $" + totalPagar);
+                            System.out.println("=============================================");
+                            System.out.println("¿Deseas calcular nuevamente? (S/N)");
+                            repetir = sc.next().charAt(0);
+
+                        } while (repetir == 'S' || repetir == 's');
+                        break;
+                    case 2:
+                        char repetir2;
+                        do {
+                            System.out.println(menu2);
+                            System.out.print("Ingrese el monto total de la cuenta: $");
+                            double montoP2 = sc.nextDouble();
+                            System.out.print("Ingrese el porcentaje de propina (por ejemplo: 15):");
+                            int porcentaje2 = sc.nextInt();
+                            System.out.print("Ingrese el número de personas: ");
+                            int numeroPersonas = sc.nextInt();
+                            System.out.println("=============================================");
+                            double propinaCalculada2 = montoP2 * porcentaje2 / 100;
+                            double totalPagar2 = montoP2 + propinaCalculada2;
+                            double montoPorPersona = totalPagar2 / numeroPersonas;
+                            System.out.println("La propina calculada es: $" + propinaCalculada2);
+                            System.out.println("El total a pagar es: $" + totalPagar2);
+                            System.out.println("Monto por persona: $" + montoPorPersona);
+                            System.out.println("=============================================");
+                            System.out.println("¿Deseas calcular nuevamente? (S/N)");
+                            repetir2 = sc.next().charAt(0);
+
+                        } while (repetir2 == 'S' || repetir2 == 's');
+                        break;
+                    case 3: 
+                        System.out.println("Saliendo...");
+                        break;
+                    default:
                     System.out.println("Opcion no valida...");
-                    break;
-            }
-        } while (!valido);
-
+                        break;
+                }
+            } while (opcion != 3);
+        }
     }
 }
